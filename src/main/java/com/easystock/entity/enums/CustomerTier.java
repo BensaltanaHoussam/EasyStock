0abf66470e86c21d4149559b7452a9e0840b5aa2
BensaltanaHoussam <bensaltna07@gmail.com>
2025-11-25 09:56:13 +0100

    add enum classes for CustomerTier, OrderStatus, PaymentStatus, and UserRole

---
+++ src/main/java/com/easystock/entity/enums/CustomerTier.java
@@ -0,0 +1,8 @@
+package com.easystock.entity.enums;
+
+public enum CustomerTier {
+    BASIC,
+    SILVER,
+    GOLD,
+    PLATINUM
+}
---
+++ src/main/java/com/easystock/entity/enums/UserRole.java
@@ -0,0 +1,6 @@
+package com.easystock.entity.enums;
+
+public enum UserRole {
+    ADMIN,
+    CLIENT
+}
